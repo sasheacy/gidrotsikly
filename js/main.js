cdcb06dd26b3b7b0810abef75fce90ce81977d79
sasheacy <sasheacysas@gmail.com>
2021-08-23 11:26:04 +0300

    +add 2 claiders

--- js/main.js
+++ js/main.js
@@ -7,14 +7,26 @@ $(function(){
     });
 
 
-    $('search__tabs-item').on('click', function(e){
+    $('.tab').on('click', function(e){
         e.preventDefault();
 
-        $('.search__tabs-item').removeClass('search__tabs-item--active');
-        $('.search__content-item').removeClass('search__content-item--active');
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
-        $(this).addClass('search__tabs-item--active');
-        $($(this).attr('href')).addClass('search__content-item--active');
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
     });
+
+    $('.product-item__favorite').on('click', function(){
+        $('.product-item__favorite').toggleClass('product-item__favorite--active')
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+    });
+
 
 });
